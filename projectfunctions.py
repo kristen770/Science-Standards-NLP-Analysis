869da1961304c90082ce6f4363054417ad149e5d
Kristen Davis <kristendavis27@gmail.com>
2021-01-21 07:39:29 -0600

    1.21.7

--- projectfunctions.py
+++ projectfunctions.py
@@ -65,14 +65,36 @@ def pdf_to_text(filepath, filename):
     with open(filename, 'w') as f:
         f.write("\n\n".join(pdf))  
 
-
-#openfile and flatten into single list item
+#single document preproccesing
 def open_and_flatten(filename): 
     """takes in a local txt file path and returns a flattened file"""
     file = open(filename) 
     yourResult = [line.split(',') for line in file.readlines()] 
     flat_text = [item for sublist in yourResult for item in sublist] 
-    return flat_text 
+    seperator = ","
+    joined_file = seperator.join(flat_text)  
+    #tokenize string
+    pattern = "([a-zA-Z]+(?:'[a-z]+)?)"
+    joined_file_tokens = nltk.regexp_tokenize(joined_file, pattern)  
+    #set all words to lower case
+    joined_file_words_lowered = [word.lower() for word in joined_file_tokens] 
+    #remove general stopwords 
+    stopwords_list = stopwords.words('english')
+    stopwords_list += list(string.punctuation)
+    stopwords_list += ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Student','Name','School',
+                       'The', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                       'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'st', 'hs', 'ess', 'ps', 'ms', 
+                      'ls', 'level', 'students', 'ex', 'wyoming', 'iv', 'ii', 'iii', 'hs', 'ls', 'ms', 'https', 
+                       'etc', 'ets',  'ess', 'ps', 'inc','rights', 'alabama', 'alaska', 'arizona', 'colorado', 
+                       'flordia', 'georgia', 'idaho', 'louisiana', 'mass', 'minnesota', 'mississippi', 
+                       'missouri', 'montana', 'nebraksa', 'northdakota', 'oklahoma', 'southcarolina', 
+                       'southdakota', 'tennessee', 'utah', 'westvirginia', 'wisconsin', 'wyoming', 'curriculum', 
+                      'grade', 'science', 'su', 'su', 'table', 'contents', 'back', 'texreg', 'january', 
+                      'feburary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 
+                      'november', 'december', 'high', 'school', 'maine', 'michigan', 'northcarolina', 'ohio', 
+                       'pennsylvania', 'texas', 'virginia', 'ch', 'appendix']
+    joined_file_words_stopped = [word for word in joined_file_words_lowered if word not in stopwords_list] 
+    return joined_file_words_stopped
 
 
 def write_multiple_pdfs_to_text(path_list, filename):  
